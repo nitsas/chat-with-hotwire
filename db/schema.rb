434929a8383e2f529654add8464cbad8e9e36ee3
Chris Nitsas <nitsas.chris@gmail.com>
2024-06-16 20:12:49 +0300

    Create Message model: bin/rails generate model message room:references content:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_16_165157) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_16_165310) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "room_id", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "rooms"
 end
